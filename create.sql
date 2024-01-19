70a1abb1d70ad2b2b66e390c416e8060374a2589
Michael Nightingale <michaeln.public@icloud.com>
2024-01-19 14:38:40 +0000

    Add db migration

--- create.sql
+++ create.sql
@@ -97,3 +97,4 @@ create table payment (amount integer, payment_id integer not null, bic varchar(2
 create table payment (amount integer, payment_id integer not null, bic varchar(255), currency varchar(255), iban varchar(255), primary key (payment_id));
 create table payment (amount integer, payment_id integer not null, bic varchar(255), currency varchar(255), iban varchar(255), primary key (payment_id));
 create table payment (amount integer, payment_id integer not null, bic varchar(255), currency varchar(255), iban varchar(255), primary key (payment_id));
+create table payment (amount integer, payment_id integer not null, bic varchar(255), currency varchar(255), iban varchar(255), primary key (payment_id));
